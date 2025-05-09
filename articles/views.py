ad15c1d4d672dbcc2a5f2b2a9ea5985fba39f8c1
ZirAjs <zirajs7@gmail.com>
2025-05-09 23:49:13 +0900

    Add article routers

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import Article
+from .serializers import ArticleSerializer
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+
+@permission_classes([AllowAny])
+class ArticleViewSet(ModelViewSet):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+    http_method_names = ["get"]
--- config/urls.py
+++ config/urls.py
@@ -6,6 +6,8 @@ from rest_framework_simplejwt.views import (
 )
 from rest_framework import permissions
 
+from articles.views import ArticleViewSet
+
 # OPEN API
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -26,6 +28,9 @@ schema_view = get_schema_view(
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("accounts.urls")),  # accounts 앱의 auth_urls.py를 포함
+    path(
+        "api/", include("articles.urls"), name="articles"
+    ),  # articles 앱의 urls.py를 포함
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     # Swagger URL
